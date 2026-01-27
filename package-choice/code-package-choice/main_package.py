5d8e941d185b411385086882eaf2cda54e930955
Tyra Krivonak <tkrivonak@axon.com>
2026-01-26 22:44:03 -0600

    log error if valid fials

--- package-choice/code-package-choice/main_package.py
+++ package-choice/code-package-choice/main_package.py
@@ -240,6 +240,7 @@ def run_automation():
             else:
                 print("VALIDATION FAILED! The value in the box does not match what we typed.")
                 print("Aborting to prevent errors.")
+                log_error(sid, lname, f"Standard Pkg: {entry_for_validation}", f"VALIDATION FAILED (Found: '{found_pkg}')")
                 return False
 
         time.sleep(0.5) # Pause between students
